2ba21fdcc26139d5abac8d16ceb71147bc9148c5
nbaiden76-netizen <>
2026-02-21 14:56:36 +0000

    List Indexing

---
+++ mastering_python_lists/list_indexing/main.py
@@ -0,0 +1,25 @@
+cities = ["Paris", "Tokyo", "New York", "Sydney", "Berlin"]
+
+# Write your code here
+second = ___
+last = ___
+
+# Testing
+print(f"The second element of the array is: {second}, the last element of the array is {last}")
+
+#cities = ["Rome", "London", "New York", "Brasilia", "Kioto"]
+
+# Getting the first element
+#print(cities[0])
+
+# Getting the third element
+#print(cities[2])
+
+#cities = ["Rome", "London", "New York", "Brasilia", "Kioto"]
+
+# Getting the last element
+#print(cities[-1])
+
+# Getting the fourth element
+# print(cities[3], cities[-2])
+
