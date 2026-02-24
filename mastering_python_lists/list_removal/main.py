a3c56ca18690e7d9a3b629753aa768864deb3677
nbaiden76-netizen <>
2026-02-24 21:22:15 +0000

    List Removal

---
+++ mastering_python_lists/list_removal/main.py
@@ -0,0 +1,15 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Deleting element
+travel_wishlist.remove("Oslo")
+travel_wishlist.remove("Sydney")
+
+# Testing
+print('Updated list:', travel_wishlist)
+
+# travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Remove a specific city
+# travel_wishlist.remove("Kyoto")
+# print(travel_wishlist)  # Output: ['Paris', 'Oslo', 'Sydney']
+
