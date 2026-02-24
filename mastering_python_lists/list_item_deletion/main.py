40193b4781869ee02fb4c2bda134e16df8257562
nbaiden76-netizen <>
2026-02-24 21:13:32 +0000

    List item deletion

---
+++ mastering_python_lists/list_item_deletion/main.py
@@ -0,0 +1,14 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney", "Rome"]
+
+# Write your code below
+del travel_wishlist[0]
+del travel_wishlist[-3]
+
+# Testing 
+print(travel_wishlist)
+
+#travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Remove the last city
+#del travel_wishlist[-1]
+#print(travel_wishlist) # Output: ['Paris', 'Oslo', 'Kyoto']
