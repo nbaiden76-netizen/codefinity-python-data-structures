6a6e4a7de8c1cc6f711b51961e740862369e5913
nbaiden76-netizen <>
2026-02-24 20:51:37 +0000

    List append

---
+++ mastering_python_lists/list_append/main.py
@@ -0,0 +1,16 @@
+travel_wishlist = ['Paris', 'Tokyo']
+
+# Write your code below
+travel_wishlist.append("Berlin")
+travel_wishlist.append("New York")
+
+
+# Testing
+print("Updated list:", travel_wishlist)
+
+# travel_wishlist = ['Paris', 'Tokyo', 'New York', 'Rome']
+
+# Adding a new city to the travel wishlist
+# travel_wishlist.append("Sydney")
+
+# print(travel_wishlist) # Output: ['Paris', 'Tokyo', 'New York', 'Rome', 'Sydney']
