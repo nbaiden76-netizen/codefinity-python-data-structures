851bfc1913ceb6aadd451a167fac55740ecc35c7
nbaiden76-netizen <>
2026-02-25 22:00:13 +0000

    Dictionary Basics

---
+++ mastering_python_dictionaries/dictionary_basics/main.py
@@ -0,0 +1,23 @@
+# Write your code here
+library = {"title": "The Great Gatsby", "author": "F.Scott Fitzgerald",
+          "title2": "1984", "author2": "George Orwell", 
+           "title3": "To Kill a Mockingbird","author3": "Harper Lee"}
+
+# Testing
+#print('Library dictionary:', library)
+
+#book = {
+#    "title": "Pride and Prejudice",
+#    "author": "Jane Austen",
+#    "year": 1813,
+#    "genre": "Romance"
+#}
+#print(book)
+
+#shelves = {1: "Classics", 2: "Science Fiction", 3: "Mystery", 4: "Non-fiction"}
+#print(shelves)
+
+#book = {"title": "1984", "author": "George Orwell", "title": "Animal Farm"}
+#print(book)  # Output: {'title': 'Animal Farm', 'author': 'George Orwell'}
+
+
