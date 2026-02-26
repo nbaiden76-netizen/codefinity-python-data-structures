7e2db27c4a40e490d5285932891170bcc3ec8d67
nbaiden76-netizen <>
2026-02-26 18:01:54 +0000

    Dictionary Deletion

---
+++ mastering_python_dictionaries/dictionary_deletion/main.py
@@ -0,0 +1,40 @@
+authors_books = {
+    'William Shakespeare': ['Hamlet', 'Macbeth', 'Romeo and Juliet', 'Othello'],
+    'J.K. Rowling': ['Harry Potter and the Sorcerer\'s Stone', 'Harry Potter and the Chamber of Secrets', 'Harry Potter and the Prisoner of Azkaban', 'Harry Potter and the Goblet of Fire'],
+    'George Orwell': ['1984', 'Animal Farm', 'Coming Up for Air'],
+    'Stephen King': ['It', 'The Shining', 'Carrie', 'Misery'],
+    'Agatha Christie': ['Murder on the Orient Express', 'The Murder of Roger Ackroyd', 'And Then There Were None', 'Death on the Nile']
+}
+
+# Write your code here
+del authors_books['J.K. Rowling']
+
+# Testing
+print("Updated dictionary:", authors_books)
+
+#book = {
+ #   "title": "Pride and Prejudice",
+ #   "author": "Jane Austen",
+ #   "year": 1813,
+ #   "genre": "Romance",
+ #   "copies": 5
+#}
+
+# Deleting a key value
+#del book["copies"]
+
+#print(book)
+
+
+#book = {
+ #   "title": "Pride and Prejudice",
+#    "author": "Jane Austen",
+ #   "year": 1813,
+ #   "genre": "Romance"
+#}
+
+# Deleting the entire dictionary
+#del book
+# Attempting to print the dictionary after deletion will cause an error
+#print(book)  # This line will raise a NameError
+
