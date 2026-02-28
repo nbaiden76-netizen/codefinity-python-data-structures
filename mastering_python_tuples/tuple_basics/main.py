7cf004c7d53799674ac52552ba34968023dce7e2
nbaiden76-netizen <>
2026-02-28 22:37:11 +0000

    Tuple Basics

---
+++ mastering_python_tuples/tuple_basics/main.py
@@ -0,0 +1,21 @@
+# Write your code here
+space_movies = ('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian',)
+
+# print(type(space_movies))
+
+# Testing
+#print("Movies about space:", space_movies)
+
+#not_a_tuple = ("Inception")
+#print(type(not_a_tuple))
+
+#empty_tuple = ()
+#rint(empty_tuple)
+#print(type(empty_tuple))
+
+#movie_title = tuple("Inception")
+#print(movie_title)
+
+#movie_list = ["Inception", "Interstellar", "Tenet"]
+#movies_tuple = tuple(movie_list)
+#print(movies_tuple)
