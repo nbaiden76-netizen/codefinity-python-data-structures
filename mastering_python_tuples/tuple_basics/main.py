11d09c522de68dc6b84a0bf0efae89dff35284eb
nbaiden76-netizen <>
2026-02-28 22:37:57 +0000

    Tuple Basics

--- mastering_python_tuples/tuple_basics/main.py
+++ mastering_python_tuples/tuple_basics/main.py
@@ -1,6 +1,8 @@
 # Write your code here
 space_movies = ('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian',)
 
+
+
 # print(type(space_movies))
 
 # Testing
