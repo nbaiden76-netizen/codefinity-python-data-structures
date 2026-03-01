eaa573d796ed88cf5cb94c7c0a24c232ec17e919
nbaiden76-netizen <>
2026-03-01 16:27:09 +0000

    Tuple addition

--- mastering_python_tuples/tuple_addition/main.py
+++ mastering_python_tuples/tuple_addition/main.py
@@ -1,6 +1,41 @@
 animal_movies = ('The Lion King', 'Jurassic Park', 'Finding Nemo')
 
 # Write your code here
-___
+# adding tuples together 
+#new_movies = ('Dumbo', 'Zootopia')
+
+#animal_movies += new_movies
+
+# Converting to lists then using append to create a tuple 
+
+new_movies = list(animal_movies)
+new_movies.append('Dumbo')
+new_movies.append('Zootopia')
+animal_movies = tuple(new_movies)
 
 print("Updated animal movies:", animal_movies)
+
+# Original tuple of movies
+#movies = ("Inception", "Interstellar", "Tenet")
+
+# Convert the tuple to a list
+#movies_list = list(movies)
+
+# Add a new movie to the list
+#movies_list.append("Dunkirk")
+
+# Convert the list back to a tuple
+#movies = tuple(movies_list)
+
+#print("After:", movies)
+
+# Original tuple of movies
+#movies = ("Inception", "Interstellar", "Tenet")
+
+# Create a new tuple with the movie to add
+#new_movies = ("Dunkirk",)
+
+# Concatenate the tuples
+#movies += new_movies
+
+#print(movies)
