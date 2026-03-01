01ba3b838d74f6d8296abfee91b1c5ebcb6b42d2
nbaiden76-netizen <>
2026-03-01 15:45:51 +0000

    Tuple Concatenation

---
+++ mastering_python_tuples/tuple_concatenation/main.py
@@ -0,0 +1,19 @@
+space_movies = ('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian')
+
+new_movies = ['The Lion King', 'Jurassic Park', 'Finding Nemo']
+
+# Write your code here
+animal_movies = tuple(new_movies)
+
+movie_poster = space_movies + animal_movies
+
+# Testing
+print("Now playing in theaters:", movie_poster)
+
+#movie_titles = ("Inception", "Interstellar", "Dunkirk")
+#release_years = (2010, 2014, 2017)
+
+# Concatenate the tuples
+#combined_tuple = movie_titles + release_years
+
+#print(combined_tuple)
