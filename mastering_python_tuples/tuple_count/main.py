0ed387d1a9c6fb32576add1a0ef62257ca7fce6a
nbaiden76-netizen <>
2026-03-01 16:36:00 +0000

    Tuple Count

---
+++ mastering_python_tuples/tuple_count/main.py
@@ -0,0 +1,24 @@
+directors = (
+    'Steven Spielberg',
+    'Christopher Nolan',
+    'Quentin Tarantino',
+    'Martin Scorsese',
+    'Steven Spielberg',
+    'Ridley Scott',
+    'James Cameron',
+    'Steven Spielberg'
+)
+
+# Write your code here
+director_count = directors.count('Steven Spielberg')
+
+# Testing
+print("The number of occurrences of the director:", director_count)
+
+# Tuple of movie genres
+#movie_genres = ("Action", "Drama", "Action", "Comedy", "Drama", "Action")
+
+# Counting occurrences of "Action"
+#action_count = movie_genres.count("Action")
+
+#print(f"'Action' appears {action_count} times in the tuple.")
