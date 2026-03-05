f3a01a4945f044034cf64e6f3e52f802afba2956
nbaiden76-netizen <>
2026-03-05 00:31:16 +0000

    Set Basics

--- mastering_python_sets/set_basics/main.py
+++ mastering_python_sets/set_basics/main.py
@@ -1,5 +1,22 @@
 # Write your code here
-nolan_movies = ___
+nolan_movies = {"Inception", "The Dark Knight", "Interstellar", "Tenet"}
 
 # Testing
 print("Nolan's movies:", nolan_movies)
+
+# Creating a set which contains strings
+#movie_set = set("Interstellar")
+#print(movie_set)
+
+# Creating a set of movies using curly braces
+#movies = {"Inception", "Interstellar", "Tenet", "Dunkirk", "Tenet"}
+#print(movies)
+
+# Using a list `[]`
+#movies = set(["Tenet", "Dunkirk", "Inception"])
+#print(movies)
+
+# Using a tuple `(,)`
+#movies = set(("Tenet", "Dunkirk", "Inception"))
+#print(movies)
+
