34d4cc4518b83fde523aae6dd8c6860206151830
nbaiden76-netizen <>
2026-03-05 21:33:54 +0000

    Updating Sets

---
+++ mastering_python_sets/updating_sets/main.py
@@ -0,0 +1,22 @@
+marvel_movies = {
+    'Avengers: Endgame',
+    'Black Panther',
+    'Iron Man'
+}
+
+movies_to_add = ('Spider-Man: No Way Home', 'Guardians of the Galaxy')
+
+# Write your code here
+marvel_movies.update(movies_to_add)
+
+# Testing
+print("Updated set:", marvel_movies)
+
+# Original set of favorite movies
+#favorite_movies = {"Inception", "Interstellar", "Tenet"}
+
+# Adding multiple new movies
+#favorite_movies.update(["Tenet", "Memento", "The Prestige"])
+
+# Print the updated set
+#print(favorite_movies)
