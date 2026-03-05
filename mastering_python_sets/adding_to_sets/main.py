5487033893a638feff469cf863244ebdf6d3671c
nbaiden76-netizen <>
2026-03-05 21:29:32 +0000

    Adding to Sets

---
+++ mastering_python_sets/adding_to_sets/main.py
@@ -0,0 +1,26 @@
+tarantino_movies = {
+    'Pulp Fiction',
+    'Kill Bill: Vol. 1',
+    'Kill Bill: Vol. 2',
+    'Inglourious Basterds',
+    'Django Unchained'
+}
+
+# Write your code here
+tarantino_movies.add("Reservoir Dogs")
+tarantino_movies.add("Once Upon a Time in Hollywood")
+
+# Testing
+print("Updated set:", tarantino_movies)
+
+# Creating an empty set
+#favorite_movies = set()
+#print("Initial set:", favorite_movies)  # Output - Initial set: set()
+
+# Adding movies to the set
+#favorite_movies.add("Inception")
+#favorite_movies.add("Interstellar")
+#favorite_movies.add("Tenet")
+#favorite_movies.add("Dunkirk")
+
+#print("Updated set:", favorite_movies)  # Output - Updated set: {'Interstellar', 'Dunkirk', 'Inception', 'Tenet'}
